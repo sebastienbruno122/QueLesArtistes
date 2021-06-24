bd20e54a45b5fea1eb114ba6a724525be4c30dad
Iñaki Gauthier <inakigauthier92@gmail.com>
2021-06-24 16:25:30 +0200

    column name delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_144437) do
+ActiveRecord::Schema.define(version: 2021_06_24_142240) do
 
   create_table "artistes", force: :cascade do |t|
     t.string "name"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2021_06_21_144437) do
 
   create_table "nouveautes", force: :cascade do |t|
     t.string "citation"
-    t.string "name"
     t.integer "artiste_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+uts "Cleaning database..."
+puts "Cleaning artistes..."
+Vote.destroy_all
+Proposition.destroy_all
+puts "Cleaning nouveautés..."
+
+
+artiste1 = Artiste.create(email: "richad@friendstrip.com", password:"123456", first_name:"Richad")

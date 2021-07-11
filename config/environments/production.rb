c1113d33c46beec06468db6f3242493c5f9d4e48
Sebastien Bruno <sebastienbruno122@gmail.com>
2021-07-11 17:03:42 +0200

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,8 +107,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-config.action_mailer.default_url_options = { :host => 'new_app_name.herokuapp.com' }
+config.action_mailer.default_url_options = { :host => 'https://quelesartistes.herokuapp.com/' }
 config.action_mailer.delivery_method = :smtp
+
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
@@ -119,5 +120,6 @@ config.action_mailer.smtp_settings = {
   user_name:            ENV["GMAIL_EMAIL"],
   password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
-  enable_starttls_auto: true  }
+  enable_starttls_auto: true
+}
 end

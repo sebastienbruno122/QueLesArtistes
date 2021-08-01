682606b14dae0b46a933a3a713240e534a05b691
Iñaki Gauthier <inakigauthier92@gmail.com>
2021-08-01 21:48:00 +0200

    iphone pratiquement done

--- db/migrate/20210801174558_tiktok_link_to_pokemons.rb
+++
@@ -1,6 +0,0 @@
-class TiktokLinkToPokemons < ActiveRecord::Migration[6.0]
-  def change
-    add_column :artistes, :tiktok_link, :string
-    add_column :artistes, :youtube_link_for_contacts, :string
-  end
-end
